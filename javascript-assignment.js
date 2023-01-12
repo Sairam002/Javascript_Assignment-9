7cb58f03d5c4d2dbcf2bf9994716b1fa89bf3ad3
Sairam002 <106730092+Sairam002@users.noreply.github.com>
2023-01-12 11:08:19 +0530

    Add files via upload

---
+++ javascript-assignment.js
@@ -0,0 +1,21 @@
+//1
+    //a 
+    "Yes"
+
+    //b 
+    100;
+
+//2
+    //Closure
+    "Accessing the inner function with the help of variable assigned to the outer function is call closure."
+    //Scope
+    "Defines the lines of code is either local scope or global scope";
+
+//3
+"Lexical scope is where the variable can access both local and global variables";
+
+//4
+"Output will be : -"
+" 0"+
+"1" + 
+" 2"
